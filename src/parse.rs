75bf153fb470e8207c6d2c7b30a32aab0dcbd7c2
SVD <vdforever@proton.me>
2024-05-08 23:01:01 +0300

    str to string

--- src/parse.rs
+++ src/parse.rs
@@ -5,20 +5,20 @@ use select::{
 
 pub struct Parse;
 
-pub struct Data<'a> {
-    pub class: &'a str,
-    pub descendant: &'a str,
-    pub attr: &'a str,
+pub struct Data {
+    pub class: String,
+    pub descendant: String,
+    pub attr: String,
 }
 
 impl Parse {
-    pub async fn get_html(html: &str, data: Data<'_>) -> Vec<String> {
+    pub async fn get_html(html: &str, data: Data) -> Vec<String> {
         let document = Document::from(html);
 
         let mut results = Vec::new();
 
-        for node in document.find(Attr("class", data.class).descendant(Name(data.descendant))) {
-            if let Some(href) = node.attr(data.attr) {
+        for node in document.find(Attr("class", data.class.as_str()).descendant(Name(data.descendant.as_str()))) {
+            if let Some(href) = node.attr(data.attr.as_str()) {
                 results.push(href.to_string());
             }
         }
@@ -26,27 +26,27 @@ impl Parse {
         results
     }
 
-    pub fn get_post() -> Data<'static> {
+    pub fn get_post() -> Data {
         Data {
-            class: "thumbnail-preview",
-            descendant: "a",
-            attr: "href",
+            class: "thumbnail-preview".to_string(),
+            descendant: "a".to_string(),
+            attr: "href".to_string(),
         }
     }
 
-    pub fn get_image() -> Data<'static> {
+    pub fn get_image() -> Data {
         Data {
-            class: "image-container note-container",
-            descendant: "img",
-            attr: "src",
+            class: "image-container note-container".to_string(),
+            descendant: "img".to_string(),
+            attr: "src".to_string(),
         }
     }
 
-    pub fn get_video() -> Data<'static> {
+    pub fn get_video() -> Data {
         Data {
-            class: "gelcomVPlayer fit-width",
-            descendant: "source",
-            attr: "src",
+            class: "gelcomVPlayer fit-width".to_string(),
+            descendant: "source".to_string(),
+            attr: "src".to_string(),
         }
     }
 }
